312fef154289f1309ed621405ced670ffe4f1e36
Allen <Allen@DESKTOP-943361S>
2021-05-31 10:51:46 +0800

    Authorization 授权

--- src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
+++ src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Acme.BookStore.Localization;
 using Acme.BookStore.MultiTenancy;
+using Acme.BookStore.Permissions;
 using Volo.Abp.Identity.Web.Navigation;
 using Volo.Abp.SettingManagement.Web.Navigation;
 using Volo.Abp.TenantManagement.Web.Navigation;
@@ -20,22 +21,14 @@ namespace Acme.BookStore.Web.Menus
 
         private async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
         {
+            //if (!MultiTenancyConsts.IsEnabled)
+            //{
+            //    var administration = context.Menu.GetAdministration();
+            //    administration.TryRemoveMenuItem(TenantManagementMenuNames.GroupName);
+            //}
+
             var administration = context.Menu.GetAdministration();
             var l = context.GetLocalizer<BookStoreResource>();
-
-            //将Book页面添加到主菜单
-            context.Menu.AddItem(
-                new ApplicationMenuItem(
-                    "BooksStore",
-                    l["Menu:BookStore"],
-                    icon: "fa fa-book"
-                ).AddItem(
-                    new ApplicationMenuItem(
-                        "BooksStore.Books",
-                        l["Menu:Books"],
-                        url: "/Books"
-                    )
-                ));
 
             context.Menu.Items.Insert(
                 0,
@@ -47,6 +40,43 @@ namespace Acme.BookStore.Web.Menus
                     order: 0
                 )
             );
+
+            #region 将Book页面添加到主菜单
+            #region 无权限控制
+            //context.Menu.AddItem(
+            //    new ApplicationMenuItem(
+            //        "BooksStore",
+            //        l["Menu:BookStore"],
+            //        icon: "fa fa-book"
+            //    ).AddItem(
+            //        new ApplicationMenuItem(
+            //            "BooksStore.Books",
+            //            l["Menu:Books"],
+            //            url: "/Books"
+            //        )
+            //    )); 
+            #endregion
+
+            #region 权限控制版本
+            var bookStoreMenu = new ApplicationMenuItem(
+                    "BooksStore",
+                    l["Menu:BookStore"],
+                    icon: "fa fa-book"
+                );
+
+            context.Menu.AddItem(bookStoreMenu);
+
+            //CHECK the PERMISSION
+            if (await context.IsGrantedAsync(BookStorePermissions.Books.Default))
+            {
+                bookStoreMenu.AddItem(new ApplicationMenuItem(
+                    "BooksStore.Books",
+                    l["Menu:Books"],
+                    url: "/Books"
+                ));
+            }
+            #endregion 
+            #endregion
 
             if (MultiTenancyConsts.IsEnabled)
             {
